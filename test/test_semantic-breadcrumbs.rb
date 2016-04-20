2fedc2d60085c5986119c1645993fef57a8de1e2
Max Lielje <max@maxlielje.co>
2016-04-20 14:13:04 +0200

    chore: change from minitest to test unit

--- test/test_semantic-breadcrumbs.rb
+++ test/test_semantic-breadcrumbs.rb
@@ -1,7 +1,7 @@
-require 'minitest/autorun'
+require 'test/unit'
 require 'semantic-breadcrumbs'
 
-class SemanticBreadcrumbsTest < Minitest::Test
+class SemanticBreadcrumbsTest < Test::Unit::TestCase
   def test_breadrumbs
     expected = %(<ol class="breadcrumbs" vocab="http://schema.org/" typeof="BreadcrumbList"><li property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" href="https://example.com/arts"><span property="name">Arts</span></a><meta property="position" content="1"></li>›<li property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" href="https://example.com/arts/books"><span property="name">Books</span></a><meta property="position" content="2"></li>›<li property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" href="https://example.com/arts/books/poetry"><span property="name">Poetry</span></a><meta property="position" content="3"></li></ol>)
 
